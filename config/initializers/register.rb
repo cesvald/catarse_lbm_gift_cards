eb06f50bdf8913871ea177cd8a1dc6dac23414db
Cesar Valderrama <valderramago@gmail.com>
2016-04-22 20:20:46 -0500

    Added form target to engine

--- config/initializers/register.rb
+++ config/initializers/register.rb
@@ -1,1 +1,1 @@
-PaymentEngines.register({name: 'lbm_gift_cards', review_path: ->(backer){ CatarseLbmGiftCards::Engine.routes.url_helpers.review_lbm_gift_card_path(backer) }, locale: 'en'})
+PaymentEngines.register({name: 'lbm_gift_cards', review_path: ->(backer){ CatarseLbmGiftCards::Engine.routes.url_helpers.review_lbm_gift_card_path(backer) }, form_path: -> (backer){ CatarseLbmGiftCards::Engine.routes.url_helpers.payment_form_lbm_gift_card_path(backer) }, locale: 'en'})
